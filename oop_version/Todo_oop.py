b1ba810ee0b46182e0fc65234552e63041b0d2ba
Minon Perera <probodha616@gmail.com>
2025-06-10 16:53:05 +0530

    json commits

--- oop_version/Todo_oop.py
+++ oop_version/Todo_oop.py
@@ -1,9 +1,15 @@
+import json
+import os
+
 class TodoList:
-    def __init__(self):
+    def __init__(self,file_path="task.json"):
+        self.file_path = file_path
         self.tasks = []
+        self.load_task()
 
     def add_task(self, task):
         self.tasks.append(task)
+        self.save_task()
         print(f'{task} Added successfully')
 
     def remove_task(self, index):
@@ -11,6 +17,7 @@ class TodoList:
         #index -= 1  # Convert to 0-based index
         if 0 <= index < len(self.tasks):
             removed = self.tasks.pop(index)
+            self.save_tasks()
             print(f'\n{removed} Removed successfully')
         else:
             print('Invalid task number')
@@ -22,4 +29,21 @@ class TodoList:
             print('Your Todo list:')
             for i, task in enumerate(self.tasks, start=1):
                 print(f'{i}. {task}')
-        print()
+
+    def save_task(self):
+        try:
+            with open (self.file_path,'w',encoding='utf-8' ) as f:
+                json.dump(self.tasks, f, indent=2)
+        except  Exception as e:
+            print(f'value saved in task {e}')  
+
+    def load_task(self):
+         if os.path.exists(self.file_path):
+             try:
+                 with open(self.file_path, 'r', encoding='utf-8') as f:
+                    self.tasks = json.load(f)
+             except Exception as e:
+                 print(f'Error loading tasks: {e}')
+                 self.tasks = []
+                     
+        #print()
